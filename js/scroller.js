29ea316556d68ba4b6f1fd789eb87b24e988305a
kolya-t <toporkov.kolya96@gmail.com>
2016-11-18 18:09:26 +0300

    Added up-button to scroll page. Added some content for history.html

---
+++ js/scroller.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+    $('#scroller').click(function () {
+        $('body,html').animate({scrollTop: 0}, 400);
+        return false;
+    });
+});
